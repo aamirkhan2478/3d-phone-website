5e8a2f699f6aa4c9426a2e3920d25cf534d7eaca
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-03-24 02:56:38 +0500

    Add GSAP animation to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,70 @@
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect } from "react";
 
-export default Hero
+const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 768 ? smallHeroVideo : heroVideo
+  );
+
+  const handleResizeSet = () => {
+    if (window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResizeSet);
+    return () => window.removeEventListener("resize", handleResizeSet);
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#title", {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+      delay: 2,
+    });
+    gsap.to("#cta", {
+      duration: 1,
+      opacity: 1,
+      y: -50,
+      delay: 2,
+    });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="title" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
