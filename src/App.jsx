d23c97667bd3e3a154c1941f65c10fdce5a5d2dc
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-03-24 02:20:53 +0500

    Add Navbar, Hero, and Highlights components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <main className="bg-black">
-      Navbar
-      Hero
-      Highlights
+      <Navbar />
+      <Hero />
+      <Highlights />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
