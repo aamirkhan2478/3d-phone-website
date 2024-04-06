bd83b6c643e889efa59fd4e1bde65c9ffbd6e61a
DarkenTech1234@ <aamir@darkentech.com>
2024-04-06 17:57:52 +0500

    Add animation and new section to Highlights component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,45 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#heading", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      direction: 1,
+      stagger: 0.2,
+    });
+  }, []);
   return (
-    <div>Highlights</div>
-  )
-}
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full flex items-center justify-between">
+          <h1 id="heading" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film <img src={watchImg} alt="watch" className="ml-2" />
+            </p>{" "}
+            <p className="link">
+              Watch the event{" "}
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
+    </section>
+  );
+};
 
-export default Highlights
+export default Highlights;
